6964c90e5b734211c4cd66e4e1a41ae8d9e2bd41
JamesRavenT <jraven.tabag@gmail.com>
2024-02-05 01:46:05 +0800

    Added Modals

--- app.js
+++ app.js
@@ -2,40 +2,66 @@
 import { getCurrentDate, getCurrentDay } from "./lib/datehelper.js";
 
 //VARIABLE DECLARATIONS
-const contentBG = document.getElementById('body');
-const doW = document.getElementById('currentDoW');
-const date = document.getElementById('currentDate');
-const time = document.getElementById('currentTime');
+const contentBG = document.querySelector('.body'),
+      nav = document.querySelector('.navbar'),
+      doW = document.getElementById('currentDoW'),
+      date = document.getElementById('currentDate'),
+      time = document.getElementById('currentTime'),
+      alarmBtn = document.getElementById('alarmBtn'),
+      worldBtn = document.getElementById('wclockBtn'),
+      comparatorBtn = document.getElementById('comparatorBtn'),
+      tools = document.querySelector('.tool_slider'),
+      addAlarmBtn = document.querySelector('#alarm_AddBtn'),
+      modal_SetAlarm = document.querySelector('.popup_SetAlarm'),
+      modal_SetAlarmOverlay = document.querySelector('.overlay'),
+      modal_SetAlarmContainer = document.querySelector('.setAlarmContainer');
 
 //MAIN
+init_NavigationBar();
 init_BackgroundAccordingToTime();
 init_Clock();
 init_Date();
-
+init_Tools();
 
 //FUNCTIONS
+
+function init_NavigationBar() {
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > 50) {
+            nav.classList.add('navbar-scrolled');
+        } else if(window.scrollY < 50) {
+            nav.classList.remove('navbar-scrolled');
+        }
+    })
+}
+
 function init_BackgroundAccordingToTime() {
 
     let hour = new Date().getHours() + 1;
-    
-    if(hour >= 1 && hour <= 4) {
-        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ')';
-    } else if (hour >= 5 && hour <= 6) {
-        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ')';
-    } else if (hour >= 7 && hour <= 10) {
-        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ')';
-    } else if (hour >= 11 && hour <= 15) {
-        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ',' + '#FFFF8F' + ')';
-    } else if (hour >= 16 && hour <= 18) {
+    if (hour >= 1 && hour <= 4) { // Night
+        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#1B1212' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ')';
+    } else if (hour >= 5 && hour <= 7) { // Early Morning
+        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#1B1212' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ')';
+    } else if (hour >= 8 && hour <= 9) { // Morning
+        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#1B1212' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ')';
+    } else if (hour >= 10 && hour <= 11) { // Late Morning
+        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#1B1212' + ',' + '#2b2b3b' + ',' + '#FFFF8F' + ')';
+    } else if (hour == 12) { // Noon 
         contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#1B1212' + ',' + '#2b2b3b' + ',' + '#F4BB44' + ')';
-    } else if (hour >= 19 && hour <= 21) {
-        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ')';
-    } else if (hour >= 22 && hour <= 24) {
-        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ',' + '#2b2b3b' + ')';
-    }
+    } else if (hour >= 13 && hour <= 15) { // Afternoon
+        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#1B1212' + ',' + '#2b2b3b' + ',' + '#F4BB44' + ')';
+    } else if (hour == 16) { // Late Afternoon
+        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#1B1212' + ',' + '#2b2b3b' + ',' + '#F4BB44' + ')';
+    } else if (hour >= 17 && hour <= 18) { // Early Evening
+        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#1B1212' + ',' + '#2b2b3b' + ',' + '#F4BB44' + ')';
+    } else if (hour >= 19 && hour <= 20) { // Evening
+        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#1B1212' + ',' + '#2b2b3b' + ',' + '#F4BB44' + ')';
+    } else if (hour >= 21 && hour <= 24) { // Night
+        contentBG.style.backgroundImage = 'linear-gradient(' + 'to bottom' + ',' + '#1B1212' + ',' + '#2b2b3b' + ',' + '#F4BB44' + ')';
+    } 
     
 }
-
 
 function init_Clock() {
     setInterval(() => {
@@ -49,3 +75,26 @@ function init_Date() {
     date.innerHTML = getCurrentDate();
 }
 
+function init_Tools(){
+    alarmBtn.addEventListener('click', () => {
+        tools.style.transform = 'translate(0%)';
+    });
+    worldBtn.addEventListener('click', () => {
+        tools.style.transform = 'translate(-33.3%)';
+    });
+    comparatorBtn.addEventListener('click', () => {
+        tools.style.transform = 'translate(-66.6%)';
+    });
+
+    init_AlarmToolFunctionalities();
+}
+
+function init_AlarmToolFunctionalities(){
+    addAlarmBtn.addEventListener('click', () => {
+        modal_SetAlarm.classList.add('active');
+    });
+
+    modal_SetAlarm.addEventListener('click', () => {
+        modal_SetAlarm.classList.remove('active');
+    });
+}   
