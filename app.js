4a09c3b2ec4855d5a48878e75a7877a3ee4d68e1
JamesRavenT <jraven.tabag@gmail.com>
2024-02-17 17:27:47 +0800

    made the design responsive

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //EXPORTS
-import { getCurrentDate, getCurrentDay } from "./lib/datehelper.js";
+import Alarm from './lib/class/alarm.js';
+import { getCurrentDate, getCurrentDay } from './lib/datehelper.js';
+
 
 //VARIABLE DECLARATIONS
 const contentBG = document.querySelector('.body'),
@@ -10,18 +12,29 @@ const contentBG = document.querySelector('.body'),
       alarmBtn = document.getElementById('alarmBtn'),
       worldBtn = document.getElementById('wclockBtn'),
       comparatorBtn = document.getElementById('comparatorBtn'),
-      tools = document.querySelector('.tool_slider'),
-      addAlarmBtn = document.querySelector('#alarm_AddBtn'),
+      tools = document.querySelector('.slideshow .slider'),
+      addAlarmBtn = document.querySelector('#alarm_CreateBtn'),
+      listOfAlarms = document.getElementById('alarm_list'),
       modal_SetAlarm = document.querySelector('.popup_SetAlarm'),
-      modal_SetAlarmOverlay = document.querySelector('.overlay'),
-      modal_SetAlarmContainer = document.querySelector('.setAlarmContainer');
+      modal_setAlarmBtn = document.querySelector('#confirmSetAlarmBtn'),
+      modal_SetAlarmOverlay = document.querySelector('.popup_SetAlarm .overlay'),
+      modal_AlarmNameInput = document.getElementById('alarmNameInput'),
+      modal_AlarmHourInput = document.getElementById('alarmTimeInputHour'),
+      modal_AlarmMinInput = document.getElementById('alarmTimeInputMinute'),
+      modal_AlarmMeridiemInput = document.getElementById('alarmTimeInputMeridiem');
+
+const alarmsArray = localStorage.getItem('alarms') ? JSON.parse(localStorage.getItem('alarms')) : []
+
+let listOfUserSavedAlarms = [];
 
 //MAIN
+
 init_NavigationBar();
 init_BackgroundAccordingToTime();
 init_Clock();
 init_Date();
 init_Tools();
+retrieveAlarmsOnDatabase();
 
 //FUNCTIONS
 
@@ -94,7 +107,93 @@ function init_AlarmToolFunctionalities(){
         modal_SetAlarm.classList.add('active');
     });
 
-    modal_SetAlarm.addEventListener('click', () => {
+    modal_SetAlarmOverlay.addEventListener('click', () => {
         modal_SetAlarm.classList.remove('active');
     });
-}   
+
+    modal_setAlarmBtn.addEventListener('click', () => {
+        let name = modal_AlarmNameInput.value;
+        let hour = modal_AlarmHourInput.value;
+        let min = modal_AlarmMinInput.value;
+        let meridiem = modal_AlarmMeridiemInput.value;
+        if((name == null || name == '') || (hour == null || hour == '') || (min == null || min =='') || meridiem == 'AM/PM') {
+            alert("Please fill in the blanks");
+        } else {
+            let order = convertHourstToMin(hour, min);
+            let check = checkIfAlarmAlreadyExists(order);
+            if(check == false) {
+                const alarm = new Alarm(name, hour, min, meridiem, order);
+                localStorage.setItem(order, JSON.stringify(alarm));
+                modal_SetAlarm.classList.remove('active');
+                retrieveAlarmsOnDatabase();
+            } else {
+                alert("The timeslot is already occupied");
+            }
+        }
+        
+    });
+
+    function convertHourstToMin(hour, min) {
+        var a = hour * 60;
+        var b = a * 60;
+        var c = min * 60;
+        var d = b + c;
+        return d;
+    }
+
+    function checkIfAlarmAlreadyExists(order) {
+        let check = false;
+        for(let i = 0 ; i < localStorage.length ; i++) {
+            var key = localStorage.key(i);
+            if(key == order) {
+                check = true;
+                return check;   
+            } else {
+                check = false;
+                return check;  
+            }
+        }
+        return check;
+    }
+}
+
+function retrieveAlarmsOnDatabase() {
+    console.log(listOfUserSavedAlarms.length);
+    if(listOfUserSavedAlarms.length != 0) {
+        while (listOfUserSavedAlarms.length > 0) {
+            listOfUserSavedAlarms.pop();
+        } 
+        for(let i = 0 ; i < localStorage.length ; i++) {
+            var key = localStorage.key(i);
+            var alarm = JSON.parse(localStorage.getItem(key.value));
+            listOfUserSavedAlarms.push(alarm);
+        }
+    } else {
+        for(let i = 0 ; i < localStorage.length ; i++) {
+            var key = localStorage.key(i);
+            console.log(key);
+            
+            var alarm = JSON.parse(localStorage.getItem(key));
+            console.log(alarm);
+            listOfUserSavedAlarms.push(alarm);
+            console.log(listOfUserSavedAlarms.length);
+        }
+        displayAlarms();
+        }
+  }
+
+  function displayAlarms() {
+
+    for(let i = 0 ; i < listOfUserSavedAlarms.length ; i++) {
+        // console.log(listOfUserSavedAlarms);
+        // var alarmObj = listOfUserSavedAlarms[i];
+        // console.log(alarmObj);
+        // var alarmItem = JSON.parse(alarmObj);
+        // console.log(alarmItem);
+          const li = document.createElement('li');
+          li.innerHTML = `
+              <h1>${i}</h1>
+          `;
+          listOfAlarms.appendChild(li);
+      };
+  }
