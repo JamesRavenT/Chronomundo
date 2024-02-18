c455c9162e45e46739d06e6c50abdd57006c88bf
JamesRavenT <jraven.tabag@gmail.com>
2024-02-18 10:57:25 +0800

    Refactored files

---
+++ script.js
@@ -0,0 +1,8 @@
+import { init_NavigationBar } from './lib/functionalities/navbar.js';
+import { init_Clock } from './lib/functionalities/clock.js';
+import { init_Tools } from './lib/functionalities/tools/_selector.js';
+
+init_NavigationBar();
+init_Clock();
+init_Tools();
+
