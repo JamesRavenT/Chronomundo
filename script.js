cc4c2b474bc92eba89cd81d6ca427914661ab935
JamesRavenT <jraven.tabag@gmail.com>
2024-02-23 08:52:48 +0800

    fixed 404

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { init_NavigationBar } from './lib/functionalities/navbar.js';
 import { init_Clock } from './lib/functionalities/clock.js';
-import { init_Tools } from './lib/functionalities/tools/_selector.js';
+import { init_Tools } from './lib/functionalities/tools/selector.js';
 
 init_NavigationBar();
 init_Clock();
